8b91b1e236ebe2f7c67e66e576751788c77441dd
kinyasunori <oneforalltk0826@gmail.com>
2022-05-11 00:38:24 +0900

    新規投稿機能修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -57,9 +57,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user_id
-      redirect_to action: :index
+    unless user_signed_in?
+      redirect_to root_path
     end
   end
 
