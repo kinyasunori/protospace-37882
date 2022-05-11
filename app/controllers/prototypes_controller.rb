f523bc8df5ab838785df94f7dc7fc9af69c8c65a
kinyasunori <oneforalltk0826@gmail.com>
2022-05-11 18:17:04 +0900

    修正依頼箇所の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_prototype, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :new, :create, :show]
 
